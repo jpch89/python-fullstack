6ef2e297dcce434a739e37eee2b615ec8707b18a
进击的团子 <jpch89@outlook.com>
2018-10-07 20:46:49 +0800

    add python OOP - 01

---
+++ darkhorse/08_面向对象基础/hm_18_伪私有属性和方法.py
@@ -0,0 +1,18 @@
+class Women:
+    def __init__(self, name):
+        self.name = name
+        self.__age = 18
+
+    def __secret(self):
+        # 在对象的方法内部
+        # 可以访问对象的私有属性！
+        print('%s 的年龄是 %d' % (self.name, self.__age))
+
+
+xiaofang = Women('小芳')
+
+# 伪私有属性
+print(xiaofang._Women__age)
+
+# 伪私有方法
+xiaofang._Women__secret()
