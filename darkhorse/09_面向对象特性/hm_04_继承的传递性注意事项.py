46d289f86ab23762a325ef2ad2d8defec5f477da
进击的团子 <jpch89@outlook.com>
2018-10-08 14:32:28 +0800

    add python OOP 02

---
+++ darkhorse/09_面向对象特性/hm_04_继承的传递性注意事项.py
@@ -0,0 +1,36 @@
+class Animal:
+    def eat(self):
+        print('吃---')
+
+    def drink(self):
+        print('喝---')
+
+    def run(self):
+        print('跑---')
+
+    def sleep(self):
+        print('睡---')
+
+
+class Dog(Animal):
+    def bark(self):
+        print('汪汪叫')
+
+
+class XiaoTianQuan(Dog):
+    def fly(self):
+        print('我会飞')
+
+class Cat(Animal):
+    def catch(self):
+        print('抓老鼠')
+
+# 创建一个哮天犬的对象
+xtq = XiaoTianQuan()
+xtq.fly()
+xtq.bark()
+xtq.eat()
+
+# 没有继承关系
+# 哮天犬的对象不能调用猫类方法
+# xtq.catch()
